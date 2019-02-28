5098980f1974f77c9eac76ccf4b72f46bbaf5b4c
Michael Howard (CYBER SECURITY) <mikehow@ntdev.microsoft.com>
2019-02-28 15:55:17 -0500

    Update Program.cs
    Major Update.
    
    Added more funky Unicode and UTF8 tests.
    Removed UTF8 string copy and now use a raw byte copy.
    Marked the comparison method to turn off optimizations.

--- TimeConstComp/Program.cs
+++ TimeConstComp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
-using System.Text;
+using System.Runtime.CompilerServices;
 
 namespace TimeConstantCompare
 {
@@ -8,32 +8,54 @@ namespace TimeConstantCompare
     {
         static void Main(string[] args)
         {
-            string[] test = {"fred", "jim",
-                             "","",
-                             ""," ",
-                             "","Hey",
-                             "Bruce","",
-                             "uehdkjhakjsd","asjdfhwe",
-                             "Jimmy!","jimmy!",
-                             "Password","Passwor"};
-            for (int i = 0; i < test.Length; i += 2)
-            {
-                bool same = TimeConstantCompare("SHA256", test[i], test[i + 1]);
-                Console.WriteLine($"String1[{test[i]}] String2[{test[i + 1]}] Same[{same}]");
-            }
+            Test("fred",        "jim",      false);
+            Test("",            "",         true);
+            Test("Mike",        "Mike",     true);
+            Test("",            " ",        false);
+            Test(" ",           "  ",       false);
+            Test("\uD802",      "\uD8ff",   false);
+            Test("Jimmy!",      "jimmy!",   false);
+            Test("\u007F",      " ",        false);
+            Test("\u007F",      "",         false);
+            Test("/mytext",     "⁄mytext",  false);
+            Test("\u00C0",      "A",        false);
+            Test("\u00C1",      "A",        false);
+            Test("\u00C2",      "A",        false);
+            Test("\u00C3",      "A",        false);
+            Test("\u00C4",      "A",        false);
+            Test("\u00C5",      "A",        false);
+            Test("\u00C6",      "A",        false);
+            Test("Security",    "Securit￥",false);
         }
 
-        static bool TimeConstantCompare(string hashName, string s1, string s2)
+        static void Test(string s1, string s2, bool expected)
         {
-            var h1 = HashAlgorithm.Create(hashName).ComputeHash(Encoding.UTF8.GetBytes(s1));
-            var h2 = HashAlgorithm.Create(hashName).ComputeHash(Encoding.UTF8.GetBytes(s2));
+            int same = TimeConstantCompare("SHA256", s1, s2);
+            bool bsame = ((same == 0) & expected);
+            Console.WriteLine($"[{s1}] == [{s2}]? {bsame}");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]        
+        static int TimeConstantCompare(string hashName, string s1, string s2)
+        {
+            var h1 = HashAlgorithm.Create(hashName).ComputeHash(GetRawBytes(s1));
+            var h2 = HashAlgorithm.Create(hashName).ComputeHash(GetRawBytes(s2));
 
             int accum = 0;
             for (int i = 0; i < h1.Length; i++)
                 accum |= (h1[i] ^ h2[i]);
 
-            return accum == 0;
+            return accum;
+        }
+
+        private static byte[] GetRawBytes(string s)
+        {
+            byte[] bytes = new byte[s.Length * sizeof(char)];
+            System.Buffer.BlockCopy(s.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
         }
     }
 }
+
+
 
